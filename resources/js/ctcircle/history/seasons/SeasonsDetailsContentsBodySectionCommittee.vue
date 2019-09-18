a98b1158daab7ab0a22a9c4bab93e126c2f88068
Andrew Blackwell <andrew@balletmecanique.eu>
2019-09-18 16:37:38 +0200

    changed columns in seasons committee

--- resources/js/ctcircle/history/seasons/SeasonsDetailsContentsBodySectionCommittee.vue
+++ resources/js/ctcircle/history/seasons/SeasonsDetailsContentsBodySectionCommittee.vue
@@ -1,40 +1,47 @@
 <template>
-    <div class="columns is-multiline">
-        <div class="column is-3" v-for="committee_member in season.committee_members">
-            <div class="card">
-                <div class="card-image">
-                    <img
-                        v-if="committee_member.person.portraits.length > 0"
-                        :src="'https://res.cloudinary.com/ctcircle/image/fetch/w_400,h_400,c_fill,g_face/https://ctc-members.dk/media/' + committee_member.person.portraits[0].file_name" alt=""
-                    >
-                    <img
-                        v-else
-                        :src="'https://res.cloudinary.com/ctcircle/image/fetch/w_400,h_400,c_fill,g_face/https://ctc-members.dk/media/unisex_silhouette.png'" alt=""
-                    >
-                </div>
-                <div class="card-content">
-                    <p class="title is-4"><a :href="'../people/'+committee_member.person.id">{{committee_member.person.first_name}} {{committee_member.person.last_name}}</a></p>
-                    <p class="subtitle is-6">{{committee_member.committee_function.name}}</p>
-                </div>
-            </div>
+  <div class="columns is-mobile is-multiline">
+    <div
+      class="column is-half-mobile is-one-third-tablet is-one-quarter-desktop"
+      v-for="committee_member in season.committee_members"
+    >
+      <div class="card">
+        <div class="card-image">
+          <img
+            v-if="committee_member.person.portraits.length > 0"
+            :src="'https://res.cloudinary.com/ctcircle/image/fetch/w_400,h_400,c_fill,g_face/https://ctc-members.dk/media/' + committee_member.person.portraits[0].file_name"
+            alt
+          />
+          <img
+            v-else
+            :src="'https://res.cloudinary.com/ctcircle/image/fetch/w_400,h_400,c_fill,g_face/https://ctc-members.dk/media/unisex_silhouette.png'"
+            alt
+          />
         </div>
+        <div class="card-content">
+          <p class="title is-4">
+            <a
+              :href="'../people/'+committee_member.person.id"
+            >{{committee_member.person.first_name}} {{committee_member.person.last_name}}</a>
+          </p>
+          <p class="subtitle is-6">{{committee_member.committee_function.name}}</p>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-  export default {
-    props:['season'],
-    data() {
-      return {
-        message: 'hello'
-      }
-    },
-    methods: {
-
-    }
-  }
+export default {
+  props: ["season"],
+  data() {
+    return {
+      message: "hello"
+    };
+  },
+  methods: {}
+};
 </script>
 <style scoped>
-    .card-content{
-        padding: 1rem;
-    }
+.card-content {
+  padding: 1rem;
+}
 </style>
