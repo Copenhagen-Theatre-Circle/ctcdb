c2f99e438752219c9b71c1a8d16c4a891862fe18
Andrew Blackwell <andrew@balletmecanique.eu>
2019-09-18 16:30:44 +0200

    changed background

--- resources/js/ctcdb/CtcdbBaseLayout.vue
+++ resources/js/ctcdb/CtcdbBaseLayout.vue
@@ -1,43 +1,40 @@
 <template>
-    <div class="has-background-light ctcdb">
-        <ctcdb-navbar/>
-        <div class="container">
-            <main>
-                <slot></slot>
-            </main>
-        </div>
+  <div class="has-background-light ctcdb">
+    <ctcdb-navbar />
+    <div class="container">
+      <main>
+        <slot></slot>
+      </main>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: [
-    'title',
-    'members'
-    ],
-    mounted() {
-        this.updatePageTitle(this.title)
-    },
-    watch: {
-        title(title) {
-            this.updatePageTitle(title)
-        }
-    },
-    methods: {
-        updatePageTitle(title) {
-            document.title = title ? `CTC – ${title}` : `Copenhagen Theatre Circle`
-        }
-    },
-}
+  props: ["title", "members"],
+  mounted() {
+    this.updatePageTitle(this.title);
+  },
+  watch: {
+    title(title) {
+      this.updatePageTitle(title);
+    }
+  },
+  methods: {
+    updatePageTitle(title) {
+      document.title = title ? `CTC – ${title}` : `Copenhagen Theatre Circle`;
+    }
+  }
+};
 </script>
 <style scoped>
-    .ctcdb {
-        min-height: 100vh;
-        height: 100%;
-    }
-    @media only screen and (max-width: 768px) {
-      .container {
-            margin-top: 4rem !important;
-        }
-    }
+.ctcdb {
+  min-height: 100%;
+  padding-bottom: 1rem;
+}
+@media only screen and (max-width: 768px) {
+  .container {
+    margin-top: 4rem !important;
+  }
+}
 </style>
